00c03732e06258106ff76df0b149a38d5d6444ab
KostiantynNaumenko <naumenko.kostiantyn@gmail.com>
2021-08-09 18:50:22 +0300

    Initialized progect

---
+++ src/test/java/guru/springframework/mysfgpetclinic/MySfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mysfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
