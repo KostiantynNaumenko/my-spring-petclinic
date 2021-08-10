200b6540cca68604ff04a55cb35e5521bed2bdd2
KostiantynNaumenko <naumenko.kostiantyn@gmail.com>
2021-08-10 18:02:38 +0300

    Adding Service Interfaces. Closes #5

---
+++ pet-clinic-data/src/main/java/guru/springframework/mysfgpetclinic/services/VetService.java
@@ -0,0 +1,15 @@
+package guru.springframework.mysfgpetclinic.services;
+
+import guru.springframework.mysfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+
+}
