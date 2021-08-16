6b4db6502b3c7dd28100212de1108ae628239366
KostiantynNaumenko <naumenko.kostiantyn@gmail.com>
2021-08-16 17:50:46 +0300

    creating pet type map service and impl. closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/mysfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.mysfgpetclinic.services;
+
+import guru.springframework.mysfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
