f5688d81812ede2b18da6720963a9812afc8fa75
KostiantynNaumenko <naumenko.kostiantyn@gmail.com>
2021-08-18 16:31:17 +0300

    map based visit service implemented. closes #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/mysfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.mysfgpetclinic.services;
+
+import guru.springframework.mysfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/mysfgpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,56 @@
+package guru.springframework.mysfgpetclinic.services.map;
+
+import guru.springframework.mysfgpetclinic.model.Pet;
+import guru.springframework.mysfgpetclinic.model.Visit;
+import guru.springframework.mysfgpetclinic.services.PetService;
+import guru.springframework.mysfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final PetService petService;
+
+    public VisitServiceMap(PetService petService) {
+        this.petService = petService;
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if(visit != null) {
+            Pet pet = visit.getPet();
+            if(pet != null) {
+                if (pet.getId() == null) {
+                    petService.save(pet);
+                }
+
+                return super.save(visit);
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
